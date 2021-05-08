2e77e8420dbb85c639cdf17e4f10f98cae42d29d
panicdragon <d.takayama.web@gmail.com>
2021-05-09 04:21:34 +0900

    feat: 🎸 update dist files

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,17 @@
 "use strict";
-function hello(name) {
-    return `Hello, ${name}!`;
+Object.defineProperty(exports, "__esModule", { value: true });
+const EntityNames_1 = require("./EntityNames");
+const Miner_1 = require("./Miner");
+function sleep(milliseconds) {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), milliseconds);
+    });
 }
-console.log(hello("World"));
+async function main() {
+    const miner = new Miner_1.Miner(EntityNames_1.ENTITY_NAMES.ENT_MINER_BOB);
+    for (let i = 0; i < 20; ++i) {
+        miner.update();
+        await sleep(800);
+    }
+}
+main();
